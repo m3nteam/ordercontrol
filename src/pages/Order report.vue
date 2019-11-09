993cb628c863fb8ffb690d15cf5c227eb49e210e
Mirko Rajovic <rajovicmirko@gmail.com>
2019-11-09 14:00:28 +0100

    dodavanje mixin-a js-functions

--- src/pages/Order report.vue
+++ src/pages/Order report.vue
@@ -1,65 +1,15 @@
 <template>
     <q-page>
-        {{ prepareReport(this.dbObj) }}
+        {{ this.prepareReport(this.dbObj) }}
     </q-page>
 </template>
 
 <script>
     import dbObjects from '../mixin/db-objects'
-    
+    import jsFunctions from '../mixin/js-functions'
+
     export default {
-        mixins: [dbObjects],
-
-        methods:{
-            prepareReport(obj){
-                let reportObj = [];
-
-                obj.forEach(obj => {
-                    let prepData = [];
-                    const picked = (({ orders }) => ({ orders }))(obj);
-                    const pickedFin = (({ orders }) => (Object.values(orders)))(picked);
-
-                    pickedFin.forEach(pic => {
-                        pic.data.forEach(fin => {
-                            prepData.push(fin);
-                        })
-                    })
-                    const myData = prepData;
-
-                    // Calculate the sums and group data (while tracking count)
-                    const reduced = myData.reduce((m, d) => {
-                        if(!m[d.code]){
-                            m[d.code] = {...d, count: 1};
-                            return m;
-                        }
-                            m[d.code].total += d.total;
-                            return m;
-                    }, {});
-                    
-                    // Create new array from grouped data and compute the average
-                    const result = Object.keys(reduced).map((k) => {
-                        const item  = reduced[k];
-                        return {
-                            code: item.code,
-                            product: item.product,
-                            total: item.total,
-                        }
-                    });
-                    
-                    let finObj = [
-                        {
-                            "id" : obj.id,
-                            "partner" : obj.name,
-                            dataSet : result
-                        }
-                    ];
-                    //console.log(JSON.stringify(finObj,null,4))
-                    reportObj.push(finObj);
-                });
-
-                return reportObj;
-            },
-        }
+        mixins: [dbObjects, jsFunctions],
     }
 </script>
 
