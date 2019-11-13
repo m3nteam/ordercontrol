4e798083df79898e425d19efebeffe1085c78bf0
Mirko Rajovic <Mirko.Rajovic@gemikro.rs>
2019-11-13 17:50:18 +0100

    dorada update partner forme

---
+++ Class/Partner.js
@@ -0,0 +1,27 @@
+class Partner {
+    constructor(partnerObj) {
+      this._id = partnerObj._id,
+      this.name = partnerObj.name,
+      this.active = partnerObj.active,
+      this.orders = partnerObj.orders
+    }
+
+    getPartnerOrder(id){
+        return this.orders.filter(order => order.id_ord == id);
+    }
+
+    getPartnerOrdersCount(){
+        return this.orders.length;
+    }
+
+    getPartnerOrdersLastId(){
+        return this.orders[this.getPartnerOrdersCount() - 1].id_ord;
+    }
+
+    insertOrder(orderObj){
+        this.orders.push(orderObj);
+    }
+  }
+
+
+export default Partner;
